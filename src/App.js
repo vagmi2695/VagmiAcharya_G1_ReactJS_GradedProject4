a0759bdf860e470e9e5838b51fb697017fdc1b8a
vagmi2695 <56200242+vagmi2695@users.noreply.github.com>
2023-05-16 18:11:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Routes from './routes';
+//import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
